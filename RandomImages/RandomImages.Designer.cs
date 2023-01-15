0848bf116acf03cc28fa4389be8789e669fe9196
Thinh Huynh <htthinh1999@gmail.com>
2023-01-15 21:42:13 +0700

    Init commits

---
+++ RandomImages/RandomImages.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace RandomImages
+{
+    partial class RandomImages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(960, 650);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnStart.Location = new System.Drawing.Point(427, 686);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(130, 50);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // RandomImages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RandomImages";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Images";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ RandomImages/RandomImages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomImages
+{
+    public partial class RandomImages : Form
+    {
+        private const int MAX_WAIT_SECOND = 20;
+        private static readonly Random _random = new Random();
+
+        public RandomImages()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            StartRandomImages();
+            await Task.Delay(MAX_WAIT_SECOND * 1000);
+            btnStart.Enabled = true;
+        }
+
+        private async void StartRandomImages()
+        {
+            string currentFolder = Path.GetDirectoryName(Application.ExecutablePath);
+            string pictureFolder = Path.Combine(currentFolder, "Anh");
+            for (int i = 0; i < MAX_WAIT_SECOND; i++)
+            {
+                int randomImageNumber = _random.Next(1, 5);
+                pictureBox.Image = Image.FromFile(Path.Combine(pictureFolder, $"{randomImageNumber}.png"));
+                await Task.Delay(1000);
+            }
+
+            pictureBox.Image= Image.FromFile(Path.Combine(pictureFolder, $"6.png"));
+        }
+    }
+}
